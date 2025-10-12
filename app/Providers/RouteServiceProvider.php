52afe6b549d888d9d66ee31929189c260b9802a6
danilkin-nikita <ni.danilkin@gmail.com>
2025-10-12 15:18:36 +0300

    init: rate limit для web

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        RateLimiter::for('web', function (Request $request) {
+            return Limit::perMinute(500)
+                ->by($request->user()?->id ?: $request->ip());
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['throttle:web'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
