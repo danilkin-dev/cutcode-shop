2c98c9dec89521dfd93dd196e97ce74542fbdf70
danilkin-nikita <ni.danilkin@gmail.com>
2025-10-14 22:44:37 +0300

    refactor: Добавлен TelegramBotApiException и настроены githooks

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Services\Telegram\Exceptions\TelegramBotApiException;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['throttle:web'])->group(function () {
     Route::get('/', function () {
-        throw new TelegramBotApiException('123');
         return view('welcome');
     });
 });
