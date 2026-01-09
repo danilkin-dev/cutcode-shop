5c4ceeb8414f3b016b883d37d4507961163550b1
Nikita Danilkin <ni.danilkin@gmail.com>
2026-01-09 14:46:53 +0300

    refactor: Order Domain

--- src/Domain/Order/Actions/NewOrderAction.php
+++ src/Domain/Order/Actions/NewOrderAction.php
@@ -4,26 +4,27 @@ namespace Domain\Order\Actions;
 
 use Domain\Auth\Contracts\RegisterNewUserContract;
 use Domain\Auth\DTOs\NewUserDTO;
-use Domain\Order\DTOs\NewOrderDTO;
+use Domain\Order\DTOs\OrderCustomerDTO;
+use Domain\Order\DTOs\OrderDTO;
 use Domain\Order\Models\Order;
 
 final class NewOrderAction
 {
-    public function __invoke(NewOrderDTO $data): Order
+    public function __invoke(OrderDTO $order, OrderCustomerDTO $customer, bool $createAccount): Order
     {
-        if ($data->createAccount) {
-            $register = app(RegisterNewUserContract::class);
+        $registerAction = app(RegisterNewUserContract::class);
 
-            $register(new NewUserDTO(
-                name: "{$data->firstName} {$data->lastName}",
-                email: $data->email,
-                password: $data->password,
+        if ($createAccount) {
+            $registerAction(NewUserDTO::make(
+                $customer->fullName(),
+                $customer->email,
+                $order->password,
             ));
         }
 
         return Order::query()->create([
-            'payment_method_id' => $data->paymentMethodId,
-            'delivery_type_id' => $data->deliveryTypeId,
+            'payment_method_id' => $order->payment_method_id,
+            'delivery_type_id' => $order->delivery_type_id,
         ]);
     }
 }
